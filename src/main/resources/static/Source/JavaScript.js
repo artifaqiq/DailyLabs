876954a725c23d6ef12344a3376fe630f1c04bb9
Vlad Shurin <vlad.shurin1997@gmail.com>
2017-02-16 16:18:39 +0300

    Add logics

---
+++ src/main/resources/static/Source/JavaScript.js
@@ -0,0 +1,9 @@
+﻿	var countryApp = angular.module('countryApp', []);
+countryApp.controller('CountryCtrl', function ($scope, $http) {
+    $http.get('http://dailylabs.herokuapp.com/api/test/diary.json').success(function (data) {
+        console.log(data.subjects[0]);
+
+        $scope.diary = data;
+    });
+});
+	
