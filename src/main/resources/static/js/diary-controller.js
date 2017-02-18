b0bb6431c86a0cc4264ae6c2343b15313f900c5d
Artur Lomako <artifaqiq@gmail.com>
2017-02-18 03:24:49 +0300

    The addition subjects and tasks

--- src/main/resources/static/js/diary-controller.js
+++ src/main/resources/static/js/diary-controller.js
@@ -1,12 +1,71 @@
 app.controller('DiaryController', ['DiaryService', function (DiaryService) {
-        var ctrl = this;
-        ctrl.diary = DiaryService.async().then(function (data) {
-            ctrl.diary = data;
-        });
+    var ctrl = this;
+    ctrl.diary = DiaryService.async().then(function (data) {
+        ctrl.diary = data;
+    });
 
-        console.log(ctrl.diary)
 
-        ctrl.addNewSubject = function () {
-            console.log("add new subject");
+
+    ctrl.addNewSubject = function (newSubjectName) {
+        //TODO Validation
+
+        if(newSubjectName) {
+            var newSubject = {};
+            newSubject.name = newSubjectName;
+            newSubject.description = "";
+            newSubject.labs = [];
+
+            ctrl.diary.subjects.push(newSubject);
+        } else {
+            //TODO Show notice
         }
-    }]);
+
+        //TODO Clear input
+    }
+
+    ctrl.addNewLab = function (subject, newLabName) {
+        //TODO Validation
+
+
+        if(newLabName) {
+            var newLab = {};
+            newLab.name = newLabName;
+            newLab.description = "";
+            newLab.passed = false;
+
+            ctrl.diary.subjects.find(function (element, index, array) {
+                if(element == subject) {
+                    element.labs.push(newLab);
+                }
+
+            })
+        } else {
+            //TODO Show notice
+        }
+
+        //TODO Clear input
+
+    }
+
+
+
+    ctrl.save = function () {
+        //TO-DO
+    }
+
+    ctrl.getJsonDiary = function () {
+        return angular.toJson(ctrl.diary)
+    }
+
+    ctrl.getPercentPasssedLabs = function (labs) {
+        if(labs.length == 0) return 0;
+
+        var countPassedLabs = labs.filter(function (lab) {
+            return lab.passed == true;
+        }).length;
+
+        return Math.floor(countPassedLabs * 100 / labs.length);
+
+    }
+
+}]);
