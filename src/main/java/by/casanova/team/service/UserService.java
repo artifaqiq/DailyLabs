045e91cccb3d570e1188cfe566ac4edef48ddf47
Artur Lomako <artifaqiq@gmail.com>
2017-03-05 03:11:34 +0300

    UserService: exists(username) method

--- src/main/java/by/casanova/team/service/UserService.java
+++ src/main/java/by/casanova/team/service/UserService.java
@@ -1,7 +1,13 @@
 package by.casanova.team.service;
 
+import by.casanova.team.models.user.User;
+
 /**
  * Created by artifaqiq on 3/5/17.
  */
 public interface UserService {
+
+    public User getById(long id);
+    public User save(User user);
+    public boolean exists(String username);
 }
--- src/main/java/by/casanova/team/service/impl/UserServiceImpl.java
+++ src/main/java/by/casanova/team/service/impl/UserServiceImpl.java
@@ -1,7 +1,38 @@
 package by.casanova.team.service.impl;
 
+import by.casanova.team.models.user.User;
+import by.casanova.team.repository.user.UserRepository;
+import by.casanova.team.service.UserService;
+import by.casanova.team.utils.auth.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
 /**
  * Created by artifaqiq on 3/5/17.
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User getById(long id) {
+        return userRepository.findOne(id);
+    }
+
+    @Override
+    public User save(User user) {
+        user.setJwtToken(JwtUtils.createToken(user.getId(), user.getUsername(),
+                new Random().nextLong()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public boolean exists(String username) {
+        return userRepository.countUsersByUsername(username) == 1;
+    }
+
 }
